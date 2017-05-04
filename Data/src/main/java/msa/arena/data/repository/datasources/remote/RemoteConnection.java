2686f08888a6999a5461be3caac379dc222f4723
Abhi Muktheeswarar <abhi@medikoe.com>
2017-05-04 15:22:24 +0530

    Set rxjava retrofit adapter to v2 & revert back to jackson

--- Data/src/main/java/msa/arena/data/repository/datasources/remote/RemoteConnection.java
+++ Data/src/main/java/msa/arena/data/repository/datasources/remote/RemoteConnection.java
@@ -13,7 +13,6 @@ import okhttp3.internal.platform.Platform;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
-import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
 /**
