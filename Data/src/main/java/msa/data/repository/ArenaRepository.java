ee42e9e679a2a3e5f6df9f99551324711a997901
Abhi Muktheeswarar <abhi@medikoe.com>
2017-08-26 03:07:19 +0530

    Try auth retry on error in pagination demo
    Success!
    Test success for both pagination & search

--- Data/src/main/java/msa/data/repository/ArenaRepository.java
+++ Data/src/main/java/msa/data/repository/ArenaRepository.java
@@ -142,7 +142,7 @@ public class ArenaRepository implements Repository {
         });*/
         //return dataStoreFactory.getRemoteDataSource().searchMoviesObservable(query);
 
-        return dataStoreFactory.getRemoteDataSourceObservable3().switchMap(new Function<ResourceCarrier<RemoteDataSource>, Observable<ResourceCarrier<LinkedHashMap<String, Movie>>>>() {
+        return dataStoreFactory.getRemoteDataSourceObservable12().switchMap(new Function<ResourceCarrier<RemoteDataSource>, Observable<ResourceCarrier<LinkedHashMap<String, Movie>>>>() {
             @Override
             public Observable<ResourceCarrier<LinkedHashMap<String, Movie>>> apply(@NonNull ResourceCarrier<RemoteDataSource> remoteDataSourceResourceCarrier) throws Exception {
                 if (remoteDataSourceResourceCarrier.status == Status.SUCCESS && remoteDataSourceResourceCarrier.data != null)
