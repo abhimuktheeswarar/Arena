ab29a9ea3c34432c2495572a322ab6414d120057
Abhi Muktheeswarar <abhi@medikoe.com>
2017-09-18 22:29:07 +0530

    Try different type of dagger dependency injection Handle usecase in different seperate modules Yet to be tested

--- ArenaApp/src/main/java/msa/arena/tryout/di/DaggerActivity.java
+++ ArenaApp/src/main/java/msa/arena/tryout/di/DaggerActivity.java
@@ -1,9 +1,24 @@
+/*
+ * Copyright 2017, Abhi Muktheeswarar
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package msa.arena.tryout.di;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -12,7 +27,6 @@ import msa.arena.R;
 import msa.arena.tryout.di.car.DaggerVehicleComponent;
 import msa.arena.tryout.di.car.Vehicle;
 import msa.arena.tryout.di.car.VehicleComponent;
-import msa.arena.tryout.di.car.VehicleModule;
 
 public class DaggerActivity extends AppCompatActivity {
 
--- Rehearsal/src/main/java/msa/rehearsal/MainActivity.java
+++ Rehearsal/src/main/java/msa/rehearsal/MainActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017, Abhi Muktheeswarar
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package msa.rehearsal;
 
 import android.content.Intent;
@@ -17,10 +33,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
